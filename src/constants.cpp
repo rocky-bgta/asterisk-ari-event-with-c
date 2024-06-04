dbc8ffeab0557d53f41d17c5b841e7af37b5ce06
Salahin <rocky.bgta@gmail.com>
2024-06-04 10:13:08 +0600

    add remote client server url

--- src/constants.cpp
+++ src/constants.cpp
@@ -1,6 +1,7 @@
 #include "constants.hpp"
 
-const std::string Constants::SERVER_URL = "http://192.168.0.132:8088";
+//const std::string Constants::SERVER_URL = "http://192.168.0.132:8088";
+const std::string Constants::SERVER_URL = "http://192.168.30.87:18088";
 const std::string Constants::ARI_USERNAME = "asterisk";
 const std::string Constants::ARI_PASSWORD = "secret";
 const std::string Constants::STASIS_APP = "my-stasis-app";
